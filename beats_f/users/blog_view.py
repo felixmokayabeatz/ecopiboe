867243e8b85a5df994267d1d4da5e7015dc9d748
Felix Mokaya <felixmokaya675@gmail.com>
2024-07-09 21:00:44 +0300

    Improved the blogs pages for list blogs and blog details

--- beats_f/users/blog_view.py
+++ beats_f/users/blog_view.py
@@ -3,8 +3,11 @@ from .blog import BlogPost
 
 def blog_list(request):
     posts = BlogPost.objects.all().order_by('-created_at')
+    for post in posts:
+        post.author_full_name = post.author.get_full_name()
     return render(request, 'blog/blog_list.html', {'posts': posts})
 
 def blog_detail(request, pk):
     post = get_object_or_404(BlogPost, pk=pk)
+    post.author_full_name = post.author.get_full_name()
     return render(request, 'blog/blog_detail.html', {'post': post})
