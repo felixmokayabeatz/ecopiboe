ceffae536d48eb04c7181df79556e8c346c67b25
Felix Mokaya <felixmokaya675@gmail.com>
2024-07-10 21:14:13 +0300

    On the land page A user can only send three texts to the Gemni AI

--- beats_f/users/blog_view.py
+++ beats_f/users/blog_view.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from .models import BlogPost
 
 def blog_list(request):
-    # Filter authors who are staff or admin
     authors = User.objects.filter(is_staff=True)
     posts = BlogPost.objects.filter(author__in=authors).order_by('-created_at')
     
