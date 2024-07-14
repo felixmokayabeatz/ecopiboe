5cddae34d251dff051374162c6d391fa2a1d139e
Felix Mokaya <felixmokaya675@gmail.com>
2024-07-14 13:51:14 +0300

    Optimizing the blog page and back end to inrease speed of loading

---
+++ ecopiboe/static/css_js/blog/blog_detail.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const currentYear = new Date().getFullYear();
+    document.getElementById('currentYear').textContent = currentYear;
+    console.log(`Current Year: ${currentYear}`);
+});
