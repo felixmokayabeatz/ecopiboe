b35d057b6a4082bf5172785c89e8b68ba9941388
Felix Mokaya <felixmokaya675@gmail.com>
2024-07-15 21:08:31 +0300

    Revert to original satate for re-athorizing

--- ecopiboe/ecopiboe_app/views.py
+++ ecopiboe/ecopiboe_app/views.py
@@ -785,26 +785,17 @@ def send_email(request):
     return render(request, 'email/send_email.html', {'user_name': request.user.get_full_name() or request.user.username, 'user_email': request.user.email})
 
 
-from google_auth_oauthlib.flow import InstalledAppFlow
-from django.shortcuts import redirect
-from django.http import JsonResponse
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
-import os
 
 @login_required(login_url='/login/')
 def google_reauthorize(request):
     try:
         client_secrets_file = settings.GOOGLE_CREDENTIALS
+        print(client_secrets_file)
 
-        # Initialize the flow using the client secrets file
         flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+        
+        creds = flow.run_local_server(port=8000, prompt='consent')
 
-        # Use HTTPS and correct domain for OAuth flow
-        redirect_uri = 'https://www.ecopiboe.com/'  # Update with your actual domain
-        creds = flow.run_local_server(port=8000, host='localhost', authorization_prompt_message='Please visit this URL: {url}', success_message='The authentication flow has completed. You may close this window.', open_browser=True)
-
-        # Save credentials to a file
         user_id = request.user.id
         token_dir = os.path.join(settings.BASE_DIR, 'user_tokens')
         if not os.path.exists(token_dir):
@@ -813,7 +804,6 @@ def google_reauthorize(request):
         with open(token_path, 'w') as token_file:
             token_file.write(creds.to_json())
 
-        # Redirect to success page
         return redirect('send_email')
 
     except PermissionError as e:
