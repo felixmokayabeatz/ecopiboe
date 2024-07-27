d2469a490532ea831df0e23fb9cde32c50844621
Felix Mokaya <felixmokaya675@gmail.com>
2024-07-27 10:25:22 +0300

    Delete files from server command improved for easy use

--- ecopiboe/ecopiboe/celery.py
+++ ecopiboe/ecopiboe/celery.py
@@ -13,19 +13,11 @@ def debug_task(self):
     print(f'Request: {self.request!r}')
     
     
-    
-# celery.py
-
-
-# Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecopiboe.settings')
 
 # Create a Celery application
 app = Celery('ecopiboe')
-
-# Load task modules from all registered Django app configs.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Autodiscover tasks from all apps in the Django project
 app.autodiscover_tasks()
 
