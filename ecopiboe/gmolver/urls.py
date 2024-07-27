16ad1a398850b7e1ea473d0573b54a0b3bbfdd96
Felix Mokaya <felixmokaya675@gmail.com>
2024-07-27 14:27:01 +0300

    Improved the main main of gmolver app and also fixed the missing links

--- ecopiboe/gmolver/urls.py
+++ ecopiboe/gmolver/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.menu_gmolver, name='menu_gmolver'),
-    path('upload/', views.upload_file, name='upload_file'),
+    path('file_analyser/', views.fileanalyser, name='fileanalyser'),
     # path('screns_gmolver/', views.screns_gmolver, name='screns_gmolver'),
 ]
